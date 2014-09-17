748848b65b535d17695a04d26361158e7bbe5649
Helin Shiah & Kyle Daugherty <pair+helin24.kyledaugherty@devbootcamp.com>
2014-09-17 15:52:57 -0500

    started user controller sign up spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-describe UsersControllers do
-  describe 'create user form' do
-    it 'has'
+describe UsersController do
+  describe 'sign up' do
+    it 'should show sign up page without session active' do
+      get :new
+      expect(response.status).to eq 200
+      expect(response).to render_template(:new)
+    end
+
+    it 'should redirect to show page if user is created successfully' do
+      get :new
+
+      # post :create, user: {}
+    end
   end
 end
