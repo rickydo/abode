83bf71f219d12d5e076b6d6bff9fa53c068716df
Helin Shiah & Kyle Daugherty <pair+helin24.kyledaugherty@devbootcamp.com>
2014-09-18 16:01:07 -0500

    Adding shoulda matchers for expense model spec

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 describe Expense do
   let(:test_expense){Expense.first}
+
+  it { should belong_to(:payer).class_name("User") }
+  it { should have_many(:user_expenses) }
+  it { should have_many(:users).through("user_expenses") }
+
   describe "#total_paid" do
     it "should return the total  paid on the expense" do
       expect(test_expense.total_paid).to eq(400)
