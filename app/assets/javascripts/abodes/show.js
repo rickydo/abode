cceaf0db4f79292b94abca2dc66a0b38097e3f9e
Brandon Manson & Zac Mitton <pair+brandonmanson.zmitton@devbootcamp.com>
2014-09-18 16:09:31 -0500

    added ajax for expenses

--- app/assets/javascripts/abodes/show.js
+++ app/assets/javascripts/abodes/show.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
-  $('form#new_expense').on("submit", function(event) {
+  $("#new-expense-form-container").on("submit", 'form#new_expense', function(event) {
     event.preventDefault();
     var action = $(this).attr('action');
     var data = $(this).serialize();
     request = $.ajax(action, {method: "post", data: data});
-    debugger;
+    request.done(function(response) {
+      $("#new-expense-form-container").html(response);
+      fetch_expenses();
+    });
+  });
 
-  })
+  var fetch_expenses = function() {
+    var action = "/abodes/expenses"
+    request = $.ajax(action, {method: "get"});
+    request.done(function(response) {
+      $("#expenses-list ul").html(response);
+    })
+  }
+
 });
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,12 +1,23 @@
 class ExpensesController < ApplicationController
   def create
     @expense = Expense.new(expense_params)
+    @expense.payer = current_user
     if @expense.save
       render partial: 'new', locals: {expense: Expense.new}
     else
-      flash.now[:error]
+      flash.now[:error] = "Expenses need a name and an amount"
       render partial: 'new', locals: {expense: @expense}
     end
+  end
+
+  # def show
+  #   @expense = Expense.find(params[:expense_id])
+  #   render partial: 'expense', locals: {expense: @expense}
+  # end
+
+  def index
+    @dwelling = Dwelling.find(current_user.dwelling_id)
+    render partial: @dwelling.expenses
   end
 
   private
